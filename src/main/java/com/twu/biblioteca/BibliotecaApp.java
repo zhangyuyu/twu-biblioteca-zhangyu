6237f8a8453bb56236d9a89100062ec65038b4b0
yuzhang <1580074674@qq.com>
2016-02-01 22:27:29 +0800

    [zhangyu] reactor book details

--- src/main/java/com/twu/biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -3,13 +3,9 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        Library library = new Library();
-        printlnWelcomeMessage();
+        Library library = new Library("Bangalore Public Library");
+        library.printlnWelcomeMessage();
         library.listBooks("src/main/resources/libraryBooks");
     }
 
-
-    private static void printlnWelcomeMessage() {
-        System.out.println("Welcome to Bangalore Public Library !\n");
-    }
 }
--- src/main/java/com/twu/biblioteca/Library.java
+++ src/main/java/com/twu/biblioteca/Library.java
@@ -3,9 +3,20 @@ package com.twu.biblioteca;
 import java.io.File;
 import java.io.IOException;
 
+import static java.lang.String.format;
 import static org.apache.commons.io.FileUtils.readFileToString;
 
 public class Library {
+
+    private String name;
+
+    public Library(String name) {
+        this.name = name;
+    }
+
+    public void printlnWelcomeMessage() {
+        System.out.println(format("Welcome to %s !\n", this.name));
+    }
 
     public void listBooks(String filePath) {
         try {
