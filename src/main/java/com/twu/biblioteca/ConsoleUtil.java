a6cead84c8c7832972073335832030c09e2ceca9
yuzhang <1580074674@qq.com>
2016-02-01 23:37:19 +0800

    [zhangyu] Invalid Menu Option   

--- src/main/java/com/twu/biblioteca/ConsoleUtil.java
+++ src/main/java/com/twu/biblioteca/ConsoleUtil.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 
 public class ConsoleUtil {
 
+    private final static String VALID_NUMBER="[0-9]|[1-9][0-9]+";
+
     public static String getInputNum() {
-        return new Scanner(System.in).nextLine();
+        String inputNumber = new Scanner(System.in).nextLine();
+
+        if (!inputNumber.matches(VALID_NUMBER)) {
+            System.out.println("Select a valid option!");
+            inputNumber = getInputNum();
+        }
+        return inputNumber;
     }
 }
